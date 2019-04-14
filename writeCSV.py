efff05d044c77049aeaf685b22810f48b0980559
Leah Yassky <lyassky@oberlin.edu>
2019-04-14 14:57:05 -0400

    Create writeCSV.py

---
+++ writeCSV.py
@@ -0,0 +1,23 @@
+import csv
+import random
+
+with open('testData.csv', mode='w') as csv_file:
+    fieldnames = ['class', 'dept', 'birth_month']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+    writer.writeheader()
+    writer.writerow({'emp_name': 'John Smith', 'dept': 'Accounting', 'birth_month': 'November'})
+    writer.writerow({'emp_name': 'Erica Meyers', 'dept': 'IT', 'birth_month': 'March'})
+
+#CS array
+cs_classes = []
+int x = len(cs_classes)
+all_classes = []
+int y = len(all_classes)
+
+#CS only:
+for i in range(6):
+	writer.writerow({cs_classes[random.randint(0, x)]}, 
+
+
+		
