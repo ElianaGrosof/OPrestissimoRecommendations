5f42e58d2d8daa35c71276784a9d1da4de0c7b20
Eliana Grosof <elig121@gmail.com>
2019-04-17 11:09:20 -0400

    recent

--- Eliana_code/test_apyroi.py
+++ Eliana_code/test_apyroi.py
@@ -1,4 +1,5 @@
 #test_apyroi.py
+#@author: Eliana Grosof, April 2019
 
 import csv
 from apyori import apriori, load_transactions
@@ -12,18 +13,19 @@ def arrayify(file):
     return results
 
 def generatesupport(dataset):
-    output = list(apriori(dataset)) #, min_confidence=0.8
+    output = list(apriori(dataset, min_support=0.6)) #, min_confidence=0.8
     print(output)
 
 def tinytest():
-    transactions = [ ['beer', 'nuts'], ['beer', 'cheese']]
-    results = list(apriori(transactions))
+    transactions = [['beer', 'nuts'], ['beer', 'cheese']]
+    results = list(apriori(transactions, min_support=0.6))
     print(results)
 
 def main():
     file = "store_data.csv"
     dataset = arrayify(file)
     #print(dataset)
-    generatesupport(dataset)
+    #generatesupport(dataset)
+    tinytest()
 
 main()
