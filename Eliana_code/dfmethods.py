31b279d65eb41b315aa532b393a17ac18bb90365
Eliana Grosof <elig121@gmail.com>
2019-05-01 03:04:44 -0400

    wrote methods to convert carts and courses into lists and dicts

---
+++ Eliana_code/dfmethods.py
@@ -0,0 +1,40 @@
+# dfmethods.py
+# This program creates a dictionary that maps the 'semcrn' to the 'short_title' of classes
+# Also contains a method, makecarts, that converts course lists to carts (lists of "semcrns", type=str)
+# @author Eliana Grosof May 2019
+
+import pandas as pd
+
+def makedict(file):
+    df = pd.read_csv(file, dtype='str', error_bad_lines=False)
+    semcrn = df["semcrn"]
+    short_title = df["short_title"]
+
+    crn_dict = dict(zip(df.semcrn, df.short_title))
+    #print(type(crn_dict))
+    #print(crn_dict)
+    return crn_dict
+
+def makecarts(file):
+    df = pd.read_csv(file, dtype='str', error_bad_lines=False)
+    cart_col = df["courses"]
+
+    carts = []
+    for c in cart_col:
+        if type(c) != type(1.0):
+            new_cart = c.split(" ")
+            carts.append(new_cart)
+
+    return carts
+
+
+def main():
+    #courses_csv = 'courses.csv'
+    #crn_dict = makedict(courses_csv)
+
+    carts_csv = 'carts.csv'
+    carts = makecarts(carts_csv)
+
+
+
+#main()
--- Eliana_code/test_mlxtend.py
+++ Eliana_code/test_mlxtend.py
@@ -1,5 +1,7 @@
 #test_mlxtend.py
 #copied and pasted code from the tutorial
+import dfmethods
+
 import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import apriori
@@ -13,6 +15,7 @@ dataset = [['Milk', 'Onion', 'Nutmeg', 'Kidney Beans', 'Eggs', 'Yogurt'],
 def makedf(dataset):
     te = TransactionEncoder()
     te_ary = te.fit(dataset).transform(dataset)
+    print("te_ary is: ", te_ary)
     df = pd.DataFrame(te_ary, columns=te.columns_)
     return df
 
@@ -22,3 +25,31 @@ def output(df):
     frequent_itemsets = apriori(df, min_support=0.6, use_colnames=True)
     frequent_itemsets['length'] = frequent_itemsets['itemsets'].apply(lambda x: len(x))
     print(frequent_itemsets)
+
+def list_duplicates(a):
+    seen = {}
+    dupes = []
+
+    for l in a:
+        for x in l:
+            if x not in seen:
+                seen[x] = 1
+            else:
+                if seen[x] == 1:
+                    dupes.append(x)
+                seen[x] += 1
+    return seen
+
+def printduplicates(dupdict):
+    for key in dupdict:
+        if dupdict[key] != 1:
+            print(key, dupdict[key])
+
+def main():
+    carts = dfmethods.makecarts("carts.csv")
+    dups = list_duplicates(carts)
+
+    df = makedf(carts)
+    output(df)
+
+main()
