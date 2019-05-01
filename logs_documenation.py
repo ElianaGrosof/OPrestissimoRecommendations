ceb13851001ad0083a7cadb441ef63a8b75a4622
Leah Yassky <lyassky@oberlin.edu>
2019-05-01 17:44:46 -0400

    Update logs_documenation.py

--- logs_documenation.py
+++ logs_documenation.py
@@ -1,7 +1,8 @@
-##DOCUMENTATION AND TEST LOG
-##Oprestissimo Recomendation Project
+# DOCUMENTATION AND TEST LOG
+# Oprestissimo Recomendation Project
+# May 2019
 @author Leah Yassky
 @author Eliana Grosof
 @author Olivia Vasquez
 
-#This log will serve 
+#This log will serve as a record of our tests, how we interacted with different files, and what our findings are
