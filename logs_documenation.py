45ce6d83e738a7f594a7cc9f4fc25a1bd11b4b5f
Leah Yassky <lyassky@oberlin.edu>
2019-05-01 19:03:17 -0400

    Update logs_documenation.py

--- logs_documenation.py
+++ logs_documenation.py
@@ -6,3 +6,16 @@
 @author Olivia Vasquez
 
 #This log will serve as a record of our tests, how we interacted with different files, and what our findings are
+
+confidence (connection between class a and class b, how much they occur together)
+lift (confidence/support, ties connection with popularity)
+support (how popular)
+
+#May1
+-- using apyori.py to 
+
+-- findings for today
+	-- there are connections between athletic classes 
+	-- support as .02
+	
+
