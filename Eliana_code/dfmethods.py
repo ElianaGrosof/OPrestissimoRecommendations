2ecc2fcb005387c8759e64662849339e577ad5ca
Eliana Grosof <elig121@gmail.com>
2019-05-01 13:57:33 -0400

    I keep changing things slightly

--- Eliana_code/dfmethods.py
+++ Eliana_code/dfmethods.py
@@ -1,10 +1,10 @@
 # dfmethods.py
-# This program creates a dictionary that maps the 'semcrn' to the 'short_title' of classes
-# Also contains a method, makecarts, that converts course lists to carts (lists of "semcrns", type=str)
+# contains methods for transforming dataframes as relevant to this project
 # @author Eliana Grosof May 2019
 
 import pandas as pd
 
+# creates a dictionary that maps the 'semcrn' to the 'short_title' of classes
 def makedict(file):
     df = pd.read_csv(file, dtype='str', error_bad_lines=False)
     semcrn = df["semcrn"]
@@ -15,6 +15,7 @@ def makedict(file):
     #print(crn_dict)
     return crn_dict
 
+# converts course lists to carts (lists of "semcrns", type=str)
 def makecarts(file):
     df = pd.read_csv(file, dtype='str', error_bad_lines=False)
     cart_col = df["courses"]
