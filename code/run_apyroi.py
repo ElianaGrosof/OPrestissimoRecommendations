0e5f69efaac3a4c8adf55c5f24a10b76a0f5516f
Eliana Grosof <elig121@gmail.com>
2020-07-05 22:22:53 -0700

    updated code and removed old files

---
+++ code/run_apyroi.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+"""
+This program generates the support, confidence, and lift of a set of "carts" of related classes
+using the apyroi implementation of the apriori algorithm.
+
+@author: Eliana Grosof, April 2019, edited July 2020
+"""
+
+import csv
+from apyori import apriori, load_transactions
+
+import pandas as pd
+
+import time
+
+#converts a csv file into an array
+#not used in current implementation but potentially useful
+def arrayify(file):
+    """
+    Converts a comma-separated file of carts into a list of lists.
+    :param file: a .csv file containing rows of carts
+    :return: a list containing lists of carts
+    """
+    results = []
+    with open(file) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',') # change contents to floats
+        for row in reader: # each row is a list
+            results.append(row)
+    return results
+
+def run_apriori(dataset, min_sup, min_confidence):
+    """
+    This function runs the apyroi implementation of the
+    apriori algorithm, and converts it to a Pandas DataFrame.
+
+    :param dataset: list of lists of carts
+    :param min_sup: minimum support
+    :param min_confidence: minimum confidence
+    :return: DataFrame containing information about support, confidence, and lift
+    """
+
+    results = list(apriori(dataset, min_support=min_sup, min_confidence=min_confidence)) #, min_confidence=0.8
+
+    df = pd.DataFrame(columns=('Items','Antecedent','Consequent','Support','Confidence','Lift'))
+
+    Support =[]
+    Confidence = []
+    Lift = []
+    Items = []
+    Antecedent = []
+    Consequent=[]
+
+    for RelationRecord in results:
+        for ordered_stat in RelationRecord.ordered_statistics:
+            Support.append(RelationRecord.support)
+            Items.append(RelationRecord.items)
+            Antecedent.append(ordered_stat.items_base)
+            Consequent.append(ordered_stat.items_add)
+            Confidence.append(ordered_stat.confidence)
+            Lift.append(ordered_stat.lift)
+
+    df['Items'] = list(map(set, Items))
+    df['Antecedent'] = list(map(set, Antecedent))
+    df['Consequent'] = list(map(set, Consequent))
+    df['Support'] = Support
+    df['Confidence'] = Confidence
+    df['Lift']= Lift
+
+    df = df[df.Lift != 1.0]
+    df.sort_values(by =['Support','Lift'], ascending = [True, False], inplace = True)
+
+    return df
+
+
+def generatecourse(df):
+    """ generates a random course from DataFrame of courses """
+    return df['Antecedent'].sample(n=1)
+
+def tinytest():
+    """ Test for apriori algorithm from apyroi. """
+    transactions = [['beer', 'nuts'], ['beer', 'cheese']]
+    results = list(apriori(transactions, min_support=0.6))
+    print(results)
+
+def main():
+    #start = time.time()
+    min_sup = 0.0006#0.004 #float(sys.argv[1]) #0.004 or 0.003 is a good number #0.025 is managable on cartsof2014
+    min_conf = 0.1 #0.5
+    carts = arrayify('../data/converted_carts.csv')#carts_list
+    sorteddf = run_apriori(carts, min_sup, min_conf)
+    pd.set_option('display.max_columns', None)
+    print(sorteddf)
+    #write to .csv file
+    sorteddf.to_csv('../data/rules_conf=0.1.csv')
+    #end = time.time()
+    #print("time is: ", start-end)
+
+main()
--- tests.py
+++
@@ -1,7 +0,0 @@
-from apyori import apriori
-
-transactions = [
-    ['beer', 'nuts'],
-    ['beer', 'cheese'],
-]
-results = list(apriori(transactions))
